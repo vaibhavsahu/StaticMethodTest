abd17753416af54674c6e5921900c009e2cfe8d7
Vaibhav Sahu <vsahu@overstock.com>
2020-12-17 00:54:04 -0700

    Using JMockit to test private static method

---
+++ src/com/vaibhav/example/OddOrEven.java
@@ -0,0 +1,7 @@
+package com.vaibhav.example;
+
+public class OddOrEven {
+    static boolean isEven(int number){
+        return number % 2 == 0;
+    }
+}
---
+++ src/com/vaibhav/example/OddOrEventTest.java
@@ -0,0 +1,20 @@
+package com.vaibhav.example;
+
+
+import mockit.Deencapsulation;
+import mockit.Mock;
+import mockit.MockUp;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class OddOrEventTest {
+
+    @Test
+    public void twoIsEven(){
+        boolean result = Deencapsulation.invoke(OddOrEven.class, "isEven", 3);
+        Assert.assertEquals(false, result);
+    }
+
+
+
+}
